1f8431bf8cf823875699383c840a79d6c4281ec6
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-26 11:53:45 -0300

    Class 388 - Testing Asynchronous Code

---
+++ src/components/Async.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import Async from './Async';
+
+describe('Async component', () => {
+  test('renders post if request succeeds', async () => {
+    // Arrange
+    render(<Async />);
+
+    // Act
+    // ...nothing
+
+    // Assert
+    const listItemElements = await screen.findAllByRole('listitem', {
+      timeout: 1000,
+    });
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
